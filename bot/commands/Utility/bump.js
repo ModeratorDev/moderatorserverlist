a4e4c76a5b2571e55248b5e89c05ddae146480c9
Glitch (cream-disco-selenium) <none>
2021-07-14 16:45:58 +0000

    🐺🏭 Checkpoint ./.gitignore:28259816/44 ./dynamic/me/servers.ejs:28259816/462 ./bot/events/guildCreate.js:28259816/2298 ./bot/events/guildDelete.js:28259816/2004 ./bot/base/BotClass.js:28259816/2

---
+++ bot/commands/Utility/bump.js
@@ -0,0 +1,50 @@
+const Command = require('@bot/base/Command');
+const Servers = require('@models/servers')
+
+class BumpCMD extends Command {
+    constructor (client) {
+      super(client, {
+        name: "bump",
+        description: "Bump a server.",
+        category: "Utility",
+        usage: "",
+        aliases: ["bumpserver", "serverbump"],
+        permLevel: "User"
+      });
+    }
+
+    async run (client, message, args, MessageEmbed) {
+        const embed = new MessageEmbed()
+          .setColor('PURPLE')
+          .setFooter(message.author.username, message.author.avatarURL())
+            .setTitle(`Moderator Servers » Bump`)
+        
+        let server = await Servers.findOne({guildid: message.guild.id}, { _id: false })
+        if(!server) { 
+            embed.setDescription('Se produjo un error, comuníquese con un administrador del sitio.')
+            return message.channel.send(embed);
+        }
+        
+        const timeremain = getTimeRemaining(server.lastbumped)
+		      if(timeremain.days == 0) 
+            if(timeremain.hours < 2) {
+                embed.setDescription(`Too early! Please come back \n${1-timeremain.hours} hours, ${59-timeremain.minutes} minutes, ${60-timeremain.seconds} seconds.`)
+                return message.channel.send(embed)
+            }
+        await Servers.updateOne({ guildid: server.guildid }, {$set: { lastbumped: new Date(Date.parse(new Date())) } })
+        
+        embed.setDescription('Bump successfully!')
+        message.channel.send(embed);
+    }
+}
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(new Date()) - Date.parse(endtime);
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  return { total, days, hours, minutes, seconds };
+}
+
+module.exports = BumpCMD;
